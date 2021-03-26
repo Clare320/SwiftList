53f133b093f6854c08394b2bd7c9993b0cc551ac
Clare320 <lilingjie1992@gmail.com>
2021-03-26 08:52:27 +0800

    layer

--- SwiftList/LoadingHUD/CustomLayerView.swift
+++ SwiftList/LoadingHUD/CustomLayerView.swift
@@ -18,20 +18,36 @@ class CustomLayerView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        backgroundColor = .cyan
         
         let cube = CGRect(x: 20, y: 20, width: 40, height: 40)
 
         let circle = UIBezierPath(roundedRect: cube, cornerRadius: 20)
-        UIColor.red.set()
-        circle.stroke()
+        circle.lineWidth = 10
         UIColor.white.set()
-        circle.fill()
+        circle.stroke()
+        
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circle.cgPath
         
         layer.addSublayer(shapeLayer)
+       
+        let processCircle = UIBezierPath(arcCenter: CGPoint(x: 40, y: 40), radius: 20, startAngle: CGFloat.pi * -0.5, endAngle: CGFloat.pi * 0, clockwise: true)
+        processCircle.lineWidth = 10
+        UIColor.green.set()
+        processCircle.stroke()
+        UIColor.white.set()
+        processCircle.fill()
+        
+        let shapeLayer2 = CAShapeLayer()
+        shapeLayer2.path = processCircle.cgPath
+        
+        layer.addSublayer(shapeLayer2)
         
     }
     
 }
+
+/**
+ 1. 控制划圈的进度
+ 
+ */
