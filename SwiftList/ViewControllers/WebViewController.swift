1c4ddee702b960796b840259d4b1412f11c238cb
Clare320 <lilingjie1992@gmail.com>
2021-04-12 08:08:01 +0800

    test web

--- SwiftList/ViewControllers/WebViewController.swift
+++ SwiftList/ViewControllers/WebViewController.swift
@@ -7,29 +7,84 @@
 
 import UIKit
 import WebKit
+import JavaScriptCore
 
 class WebViewController: BaseViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    
+    private let scriptHandler = LJScriptMessageHandler()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        setUserContentController()
+        configureWebView()
         
         let url = URL(string: "http:192.168.1.102:3000/")
         let request = URLRequest(url: url!)
         self.webView.load(request)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setUserContentController() {
+        let userContentController = webView.configuration.userContentController
+        // post message
+        userContentController.add(scriptHandler, name: "test")
+//        userContentController.add(scriptHandler, name: "finish")
+        
+        // inject script
+        let scriptStr = """
+            (() => {
+                console.log('inject script');
+            })();
+        """
+        let script = WKUserScript(source: scriptStr, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        userContentController.addUserScript(script)
     }
-    */
+    
+    func configureWebView() {
+        webView.navigationDelegate = self
+        
+        
+    }
+    
+    func configureJSCoreBlock() {
+        // 获取当前WebView的JSContext
+//        JSContext
+    }
+    
+    
+    deinit {
+        webView.configuration.userContentController.removeAllScriptMessageHandlers()
+    }
 
 }
+
+extension WebViewController {
+    func printName(name: String) {
+        print("name: \(name)")
+    }
+    
+    func test() {
+        
+    }
+}
+
+// WKNavigationDelegate
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("\(#line)--\(#function)")
+    }
+    
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            // URL
+        let result = DecidePolicyHandler.checkNavigationAction(webView: webView, action: navigationAction)
+        decisionHandler(result)
+    }
+}
+
